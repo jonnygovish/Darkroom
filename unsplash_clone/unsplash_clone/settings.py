7c52837d0e5cfda493a174b6f7c8e9fc63f0e0cf
John <jonnygovish@gmail.com>
2017-11-17 14:48:58 +0300

    Linking the app with the projects configurations

--- unsplash_clone/unsplash_clone/settings.py
+++ unsplash_clone/unsplash_clone/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'darkroom.apps.DarkroomConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
