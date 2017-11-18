b32c4c7a1774814c6104e7af992cadff8b2beb38
John <jonnygovish@gmail.com>
2017-11-18 12:35:20 +0300

    view functions for tags and photo

--- unsplash_clone/darkroom/views.py
+++ unsplash_clone/darkroom/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 from .models import Photo,Tag
 
 # Create your views here.
@@ -6,3 +7,19 @@ def index(request):
   photos = Photo.get_photo()
   tags = Tag.get_tags()
   return render(request,'index.html',{"photos": photos, "tags": tags} )
+
+def photo(request,photo_id):
+  try: 
+    photo = Photo.objects.get(id = photo_id)
+  except DoesNotExist:
+    raise Http404()
+  return render(request,'photo.html', {"photo": photo})
+
+
+def tags(request,tag_id):
+  try: 
+    tag = Tag.objects.get(id = tag_id)
+    photos = Photo.objects.filter(tag =tag).all()
+  except DoesNotExist:
+    raise Http404()
+  return render(request,'tag.html', {"tag": tag, "photos":photos})
