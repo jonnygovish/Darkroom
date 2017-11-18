2ec160a4f77f20e6ebe6b20a52e2829875115478
John <jonnygovish@gmail.com>
2017-11-18 09:52:33 +0300

    rendering photos and tags in the indexpage view function

--- unsplash_clone/darkroom/views.py
+++ unsplash_clone/darkroom/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Photo,Tag
 
 # Create your views here.
 def index(request):
-  return render(request,'index.html')
+  photos = Photo.get_photo()
+  tags = Tag.get_tags()
+  return render(request,'index.html',{"photos": photos, "tags": tags} )
