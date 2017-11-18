dcac0c0625c0cf999c9496897b794390d4cf744a
John <jonnygovish@gmail.com>
2017-11-18 09:51:33 +0300

    Registering models int django admin

--- unsplash_clone/darkroom/admin.py
+++ unsplash_clone/darkroom/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User,Photo,Tag
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Photo)
+admin.site.register(Tag)
+
