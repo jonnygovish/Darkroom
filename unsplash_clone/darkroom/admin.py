c02a0b4e11b8bc1f8a170220cedaa00a9bb304de
John <jonnygovish@gmail.com>
2017-11-18 12:34:25 +0300

    modifying tags

--- unsplash_clone/darkroom/admin.py
+++ unsplash_clone/darkroom/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import User,Photo,Tag
 
 # Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+  filter_horizontal = ('tag',)
+
 admin.site.register(User)
-admin.site.register(Photo)
+admin.site.register(Photo,PhotoAdmin)
 admin.site.register(Tag)
 
